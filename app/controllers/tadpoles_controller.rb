9a15570a951fc64d18e4adb8232e2d76d23bbaf6
justrmarks <blackbirdwanderer@gmail.com>
2019-09-26 11:33:51 -0500

    Done.

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,6 +2,16 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphose]
 
   # add your metamorphose action here
+  def metamorphose
+    @frog = Frog.new(name: @tadpole.name, pond: @tadpole.pond, color: @tadpole.color)
+
+    if @frog.save
+      @tadpole.destroy
+      redirect_to @frog
+    else
+    end
+
+  end
 
   def index
     @tadpoles = Tadpole.all
